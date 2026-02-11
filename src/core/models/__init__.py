52a02a590eebc96e9a7401c1cdbf268bf05b7025
Sorin <sever46282@gmail.com>
2026-02-11 15:30:25 +0300

    Starting work with FastAPI-Users

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,9 +1,7 @@
 __all__ = (
     'db_helper',
     'Base',
-    'User'
 )
 
 from .db_helper import db_helper
 from .base import Base
-from .user import User
--- src/core/models/user.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-
-from .base import Base
-from .mixins.int_id_pk import IntIdPkMixin
-
-
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
