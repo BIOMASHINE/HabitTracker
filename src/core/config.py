d11fbd1758635618eb5e1f95c8e39ded1e7d40bb
Sorin <sever46282@gmail.com>
2026-02-13 20:10:05 +0300

    Create auth router

--- src/core/config.py
+++ src/core/config.py
@@ -3,16 +3,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
-    host: str = '0.0.0.0'
+    host: str = "0.0.0.0"
     port: int = 8000
 
+
 class ApiV1Prefix(BaseModel):
-    prefix: str = '/v1'
-    users: str = '/users'
+    prefix: str = "/v1"
+    auth: str = "/auth"
+
 
 class ApiPrefix(BaseModel):
-    prefix: str = '/api'
+    prefix: str = "/api"
     v1: ApiV1Prefix = ApiV1Prefix()
+
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
@@ -22,28 +25,31 @@ class DatabaseConfig(BaseModel):
     max_overflow: int = 10
 
     naming_conventions: dict[str, str] = {
-        'ix': 'ix_%(column_0_label)s',
-        'uq': 'uq_%(table_name)s_%(column_0_name)s',
-        'ck': 'ck_%(table_name)s_%(constraint_name)s',
-        'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
-        'pk': 'pk_%(table_name)s'
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
     }
+
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
     reset_password_token_secret: str
     verification_token_secret: str
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=('.env.template', '.env'),
+        env_file=(".env.template", ".env"),
         case_sensitive=False,
-        env_nested_delimiter='__',
-        env_prefix='HABIT_TRACKER__',
+        env_nested_delimiter="__",
+        env_prefix="HABIT_TRACKER__",
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
     access_token: AccessToken
+
 
 settings = Settings()
