a57bd3506efd1a7ed93143749e362db1a1d31c33
Sorin <sever46282@gmail.com>
2026-02-15 20:36:37 +0300

    Move sending verification emails to the appropriate folder

---
+++ src/mailing/email_verification/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    'send_verification_email',
+    'send_email_confirmed'
+)
+
+from .send_verification_email import send_verification_email
+from .send_email_confirmed import send_email_confirmed
