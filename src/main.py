668473a2435fa509554e71ac25faf27b5ef3d269
Sorin <sever46282@gmail.com>
2026-03-10 18:24:11 +0300

    Fix url path in middlware

--- src/main.py
+++ src/main.py
@@ -32,8 +32,8 @@ main_app.include_router(
 main_app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://127.0.0.1:5500",
-    "https://track-your-life.netlify.app/"
+        "http://127.0.0.1:5500",
+        "https://track-your-life.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
